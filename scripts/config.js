f1e5fa419e7b3c0ff823b0bf6304aa2bb8a5ffd5
dmorales414 <david.morales2889@gmail.com>
2020-09-05 12:28:59 -0400

    Testing removing git from the project.

--- scripts/config.js
+++ scripts/config.js
@@ -18,24 +18,24 @@ if (shell.rm("-rf", "../.git/").code !== 0) {
 }
 
 // Ask if the user want a new git repo
-process.stdout.write(`\nDo you want a new Git repository? [Y/n] `);
-process.stdin.resume();
-process.stdin.on("data", (pData) => {
-  const answer = pData.toString().trim().toLowerCase() || "y";
-  if (answer == "y") {
-    shell.echo(`\nWe are adding Git to your project.... ✅`);
-    shell.exec("git init");
-  } else if (answer == "n") {
-    shell.echo(
-      `\nYou can later run "git init" to use Git source control on your project.`
-    );
-    shell.exit(1);
-  } else {
-    shell.echo(
-      `\nSomething went wrong, run "git init" command if you want to use Git source control on your project.`
-    );
-    shell.exit(1);
-  }
-});
+//process.stdout.write(`\nDo you want a new Git repository? [Y/n] `);
+//process.stdin.resume();
+//process.stdin.on("data", (pData) => {
+  //const answer = pData.toString().trim().toLowerCase() || "y";
+  //if (answer == "y") {
+    //shell.echo(`\nWe are adding Git to your project.... ✅`);
+    //shell.exec("git init");
+  //} else if (answer == "n") {
+    //shell.echo(
+      //`\nYou can later run "git init" to use Git source control on your project.`
+    //);
+    //shell.exit(1);
+ // } else {
+   // shell.echo(
+     // `\nSomething went wrong, run "git init" command if you want to use Git source control on your project.`
+   // );
+    //shell.exit(1);
+  //}
+//});
 
 // Message to start working
